dbb79d4790e0ff46d5953d41aa04cd1f4b7d8d83
Ellidian-KG <113087937+Ellidian-KG@users.noreply.github.com>
2024-03-12 19:31:40 +0500

    Add files via upload

---
+++ 123123Client/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _123123Client
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        public Form1()
+        {
+            InitializeComponent();
+
+            client = new TcpClient("192.168.0.105", 12345);
+            stream = client.GetStream();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         
+         if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                string message = $"{textBox3.Text}: {textBox1.Text}\n";
+                SendMessage(message);
+                textBox1.Clear(); 
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox3.Text.Trim();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox3.Text))
+            {
+                string userNameMessage = textBox3.Text; 
+                SendMessage(userNameMessage);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void SendMessage(string message)
+        {
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                await stream.WriteAsync(data, 0, data.Length);
+
+                string formattedMessage = $"{message}\n";
+
+                textBox2.AppendText(formattedMessage);
+                textBox2.AppendText(Environment.NewLine);
+                textBox2.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error sending message: " + ex.Message);
+            }
+        }
+
+
+    
+    }
+}
---
+++ 12321Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        TcpListener server = null;
+        try
+        {
+            Int32 port = 12345;
+            IPAddress localAddr = IPAddress.Parse("192.168.0.105"); 
+
+            server = new TcpListener(localAddr, port);
+
+            server.Start();
+            Console.WriteLine("Сервер запущен...");
+
+            while (true) 
+            {
+                TcpClient client = server.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                StreamReader reader = new StreamReader(stream);
+                StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        string message = reader.ReadLine();
+                        Console.WriteLine("Получено сообщение от клиента: " + message);
+
+                        writer.WriteLine(message); 
+                    }
+                });
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        finally
+        {
+            server?.Stop();
+        }
+    }
+}
